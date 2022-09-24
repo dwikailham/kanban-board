44554d7b9da0391f85ef9b6c05be9e613a396c13
Dwika Ilham Zakaria <dwika.zakaria@ist.id>
2022-09-24 10:33:20 +0700

    fix sidebar

--- src/container/Kanban/index.js
+++ src/container/Kanban/index.js
@@ -7,7 +7,7 @@ export default function Kanban() {
     return (
         <div className='flex container'>
             <Sidebar />
-            <div className='p-10' >
+            <div className='pt-10 pl-[120px]' >
                 <h1 className='text-xl font-bold'>Product Roadmap</h1>
                 <Content />
             </div>
